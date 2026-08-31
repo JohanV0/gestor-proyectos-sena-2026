e31863b8516d6c8b5a32005ffcc174f4e3add7a2
VJ <johan.vergara0509x@gmail.com>
2026-08-31 18:50:30 -0500

    modificacion base.html

--- accounts/apps.py
+++ accounts/apps.py
@@ -2,8 +2,4 @@ from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
-<<<<<<< HEAD
-=======
-    default_auto_field = 'django.db.models.BigAutoField'
->>>>>>> login
     name = 'accounts'
